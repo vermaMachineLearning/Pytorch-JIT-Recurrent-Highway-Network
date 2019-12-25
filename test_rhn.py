dacf111011f13f99e1772a9eff8a767cbdcd0d00
Saurabh Verma <verma@cs.umn.edu>
2019-12-25 11:48:30 -0600

    Create test_rhn.py

---
+++ test_rhn.py
@@ -0,0 +1,15 @@
+from jit_recurrent_highway_network import RHN
+
+batch = 2
+seq_len = 1000
+input_dim = 10
+hidden_dim = 20
+nb_rhn_layers = 5
+
+
+input = torch.randn(batch, seq_len, input_dim)
+rhn = RHN(input_dim, hidden_dim, nb_rhn_layers, drop_prob=0.1)
+
+rhn.train()
+s_t_0_l_0 = torch.zeros(batch, hidden_dim)
+output = rhn(input, s_t_0_l_0)
